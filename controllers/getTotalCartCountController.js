441b83430bb3a4c75ac65406f6f08d13d0a356ce
Mehadi-Hasan77 <mehedihassan6838@gmail.com>
2025-10-31 22:57:04 +0600

    slow loading problem

--- controllers/getTotalCartCountController.js
+++ controllers/getTotalCartCountController.js
@@ -2,13 +2,23 @@ import pool from "../config/postgreSQL.js";
 
 export const getTotalCartCountController = async (req, res) => {
   try {
-    // const rawUserId = req.query.user_id;
     // const user_id = Number(rawUserId);
-//where user_id=$1`,[user_id]
-    const {rows} = await pool.query(
-      `SELECT SUM(quantity) AS totalQuantity FROM shopping_cart `
+    //where user_id=$1`,[user_id]
+    // const {rows} = await pool.query(
+    //   `SELECT SUM(quantity) AS totalQuantity FROM shopping_cart `
+    // );
+
+    const user_id = Number(req.query.user_id);
+   
+    if (!user_id || isNaN(user_id)) {
+      return res.status(400).json({ message: "Invalid or missing user_id" });
+    }
+
+    const { rows } = await pool.query(
+      `SELECT COALESCE(SUM(quantity), 0) AS totalquantity FROM shopping_cart WHERE user_id = $1`,
+      [user_id]
     );
-    const count = rows[0].totalQuantity || 0;
+    const count = rows[0].totalquantity || 0;
 
     return res.status(200).json({
       count: count,
